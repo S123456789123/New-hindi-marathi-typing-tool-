361f8fecb25c07398d168b334e77f222c93e7fd1
S123456789123 <sayyadtalha321@gmail.com>
2025-02-15 13:56:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.getElementById("inputBox").addEventListener("input", function() {
+    let input = this.value;
+    let convertedText = transliterate(input); // Transliteration Function Call
+    document.getElementById("outputText").innerText = convertedText;
+});
 
+// बेसिक ट्रांसलिटरेशन फंक्शन (इसे कस्टम लॉजिक से सुधार सकते हैं)
+function transliterate(text) {
+    let mapping = {
+        "a": "अ", "b": "ब", "c": "क", "d": "द", "e": "ए",
+        "h": "ह", "i": "इ", "k": "क", "m": "म", "n": "न",
+        "p": "प", "r": "र", "s": "स", "t": "ट", "u": "उ"
+    };
+
+    return text.split("").map(char => mapping[char] || char).join("");
+}
